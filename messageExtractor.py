f1ff6757b06f0ff8e8f193c6b6146beaf9112a31
WeiKuoWei <ck3294@nyu.edu>
2024-06-25 21:26:25 +0800

    feat: list all the channel names and ids and save conversations

--- messageExtractor.py
+++ messageExtractor.py
@@ -1,20 +1,18 @@
 import json
 import re
 import os
-
 from datetime import datetime as dt
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
-
+import argparse
 
 # Load environment variables from .env file
 load_dotenv()
-slack_token = os.getenv("SLACK_API_TOKEN")
+SLACK_USER_TOKEN = os.getenv("SLACK_USER_TOKEN")
 
 # Set your Slack API token
-# slack_token = env_variables["SLACK_API_TOKEN"]
-client = WebClient(token=slack_token)
+client = WebClient(token=SLACK_USER_TOKEN)
 
 def extract_reactions(reactions):
     extracted_reactions = []
@@ -65,20 +63,62 @@ def extract_messages(channel_id):
         })
     return extracted_messages
 
-def save_to_json(data, file_path):
+def save_to_json(data, name, path):
+    file_path = os.path.join(path, name)
     with open(file_path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
+def get_channel_id(channel_name):
+    cursor = None
+    try:
+        while True:
+            response = client.conversations_list(limit=1000, cursor=cursor)
+            channels = response['channels']
+            for channel in channels:
+                if channel['name'] == channel_name:
+                    print(f"Found channel {channel_name} with id {channel['id']}")
+                    return channel['id']
+            cursor = response.get('response_metadata', {}).get('next_cursor')
+            if not cursor:
+                break
+    except SlackApiError as e:
+        print(f"Error: {e.response['error']}")
+
+def list_all_channels():
+    cursor = None
+    channels = []
+    try:
+        while True:
+            response = client.conversations_list(limit=1000, cursor=cursor)
+            channels.extend(response['channels'])
+            cursor = response.get('response_metadata', {}).get('next_cursor')
+            if not cursor:
+                break
+    except SlackApiError as e:
+        print(f"Error retrieving channels: {e.response['error']}")
+        return []
+
+    channel_names = [channel['name'] for channel in channels]
+    return channel_names
+
 def main():
-    # Set the ID of the public Slack channel you want to read 
-    # NOTES: will need to automate this process, find all channel IDs, and save them into a json file
-    channel_id = 'C079049992M'
+    channel_names = list_all_channels()
+    channel_ids = []
+    print("List of all channels:")
+    for name in channel_names:
+        print(name)
+        id = get_channel_id(name)
+        channel_ids.append(id)
+
+    # file path
+    path = "dumps/conversations/"
 
     # Extract the messages from the channel
-    messages = extract_messages(channel_id)
+    for i, channel_id in enumerate(channel_ids):
+        messages = extract_messages(channel_id)
 
-    # Save the messages to a JSON file
-    save_to_json(messages, 'slack_messages.json')
+        # Save the messages to a JSON file
+        save_to_json(messages, f'{channel_names[i]}.json', path)
 
 if __name__ == '__main__':
     main()
