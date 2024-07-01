383e1c688e119415dc1723af77b705d652c54ec0
WeiKuoWei <ck3294@nyu.edu>
2024-07-01 23:23:18 +0800

    feat: create the first collection for workspace id reference and a simple notebook to monitor ChromaDB

---
+++ createdbChroma.py
@@ -0,0 +1,26 @@
+import chromadb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# create your own DB_PATH in a .env file to store chromadb data
+DB_PATH = os.getenv("DB_PATH") 
+
+def main():
+    # Connect to ChromaDB
+    client = chromadb.PersistentClient(path=DB_PATH)
+    client.get_or_create_collection(name = "team_ids")
+    print("team_ids collection created in ChromaDB")
+
+    # if we have to reset the database, all other collections will be
+    # initialized here in the future
+
+if __name__ == '__main__':
+    main()
+
+'''
+Note that only the team_ids collection is created in createdbChroma.py. This is
+because all the other collections are 1-to-many relationships and are created in
+the appChroma.py file.
+'''
