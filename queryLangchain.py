51068d30d77c0ab5e5f576bb0e9e306974a25d64
WeiKuoWei <ck3294@nyu.edu>
2024-07-06 16:11:00 +0800

    refactor: remove unnecessary comments

--- queryLangchain.py
+++ queryLangchain.py
@@ -4,6 +4,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
+from langchain.chains.question_answering import load_qa_chain
 from crudChroma import CRUD
 
 # Load environment variables
@@ -13,7 +14,6 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Initialize the OpenAI embeddings model
-# embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=OPENAI_API_KEY)
 embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # Initialize the ChromaDB client and retriever
@@ -23,29 +23,34 @@ chroma_client = Chroma(
     collection_name="C073U7920TZ"
 )
 
-# crud = CRUD()
-# chroma_client = crud.get_client()
-
 # Initialize the language model
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=500, openai_api_key=OPENAI_API_KEY)
+chain = load_qa_chain(llm, chain_type="stuff", verbose=True)
+
+# query = "What are the people in this research group trying to achieve?"
+query = "What is going on in the channel recently?"
+# query = "How are the team members feeling recently?"
+# query = "Give me an update on the project progress recently."
+# query = "How the team members in the project collaborate with each other?"
+
+prompt = f'''
+    {query} Write at least 100 words to answer the question. 
+'''
+
+matching_docs = chroma_client.similarity_search(query, k=10)
+answer = chain.run(input_documents=matching_docs, question=prompt)
+print(answer)
+
+''' Here is an alternative way to run the query, which I have not seen apparent difference in the results.
 
 # Initialize the RetrievalQA chain
 chatbot_chain = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=chroma_client.as_retriever(search_kwargs={"k": 2})
+    retriever=chroma_client.as_retriever(search_kwargs={"k": 10}), 
+    verbose=True,
 )
 
-# Define the prompt template
-template = """
-respond as clearly as possible {query}?
-"""
-
-prompt = PromptTemplate(
-    input_variables=["query"],
-    template=template,
-)
-
-# Run the query through the chatbot chain
-response = chatbot_chain.invoke(prompt.format(query="Summarize what is going on in this channel?"))
-print(response)
+answer = chatbot_chain.run(query)
+print(answer)
+ '''
