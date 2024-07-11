b888dc7825c2318a7ca461c05433f08c6de53c53
WeiKuoWei <ck3294@nyu.edu>
2024-07-11 08:01:48 +0800

    chore: update the dependencies after discord integration and add a config.py file for retrieving .env info

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 ## Project Description
 [Repository Update: 2024-07-03]
 It is decided that the Chatbot should be installed on both Slack and Discord. Thus, a more general approach is needed to make the Chatbot work on both platforms. Message extracting files for Slack and Discord will be created from the old `messageExtractor.py`, and the original file will be replaced with `getMessageSlack.py` and `getMessageDiscord.py`. `modelsChroma.py` will be deleted, while other `.py` files will be updated to work with the new message extracting files. 
-
--- src/utlis/config.py
+++ src/utlis/config.py
@@ -4,5 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-CHATGPT_TOKEN = os.getenv("CHATGPT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DB_PATH = os.getenv("DB_PATH")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+if not DISCORD_TOKEN:
+    raise ValueError("DISCORD_TOKEN is not set in .env file")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY is not set in .env file")
+
+if not DB_PATH:
+    raise ValueError("DB_PATH is not set in .env file")
+
+if not CHANNEL_ID:
+    raise ValueError("CHANNEL_ID is not set in .env file")
