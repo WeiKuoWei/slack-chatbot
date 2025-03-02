90a42562a3223cd7804c7e2ca4b28afd4b3cce14
WeiKuoWei <ck3294@nyu.edu>
2025-03-02 17:55:54 +0400

    refactor: creating files for utterances and prompts

---
+++ src/utlis/prompts.py
@@ -0,0 +1,14 @@
+CHANNEL_SUMMARIZER = '''
+    You are a channel messages summarizer. You will be the most relevant
+    messages to the user query. Answer the user as detailed as possible.
+'''
+
+COURSE_INSTRUCTOR = '''
+    You are a course instructor. You will be given the most relevant 
+    course materials to the user query. Answer the user as detail as possible.
+'''
+
+PROMPTS = {
+    'channel_summarizer': CHANNEL_SUMMARIZER,
+    'course_instructor': COURSE_INSTRUCTOR
+}
